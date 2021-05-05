d6a2bb6f45507f7b9cf8ff59508a2df4aef84b38
Balogun Hammed Olasunkanmi <mrhammedbalogun@gmail.com>
2021-05-05 13:09:57 +0100

    Fix static path error

--- theblog/settings.py
+++ theblog/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
+
+All Notice
 """
 import os
 from decouple import config
